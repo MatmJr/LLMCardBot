8ca5eed6d8db4b5183f97575149aa0ef16b817b2
Marco Mialaret Junior <matmj@cin.ufpe.br>
2024-11-24 17:11:15 -0300

    Testes com o fine-tunnig e estruturação do projeto

--- PrimeirosEstudos/AnswerBot/fine_tuning.py
+++ PrimeirosEstudos/AnswerBot/fine_tuning.py
@@ -7,13 +7,15 @@ _ = load_dotenv()
 client = openai.Client()
 
 if __name__ == "__main__":   
-    
+    """
+            Planejar muito bem essa etapa, aqui o custo pode subir muito.
+    """
     file = openai.files.create(
         file = open('./AnswerBot/treinamento/chatbot_respostas.jsonl', 'rb'),
         purpose = 'fine-tune'
     )
-
+    
     client.fine_tuning.jobs.create(
         training_file = file.id,
-        model = "gpt-3.5-turbo"
+        model = "gpt-4o-mini-2024-07-18"
     )
